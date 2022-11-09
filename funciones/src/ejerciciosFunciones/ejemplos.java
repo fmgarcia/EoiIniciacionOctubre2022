7be1115bae9ae69524980d0702417e233223ca8d
fran <ctefran@hotmail.com>
2022-11-09 18:19:23 +0100

    OperacionesFechas

--- funciones/src/ejerciciosFunciones/ejemplos.java
+++ funciones/src/ejerciciosFunciones/ejemplos.java
@@ -1,8 +1,12 @@
 package ejerciciosFunciones;
 
+import java.time.Duration;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.Period;
 import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+import java.time.temporal.Temporal;
 import java.util.Arrays;
 import java.util.Scanner;
 
@@ -45,6 +49,11 @@ public class ejemplos {
 		return cadena;
 	}
 	
+	public static long diasVividos(LocalDate fechaNacimiento) {
+		// int resta = LocalDate.now() - fechaNacimiento;  // no se puede hacer
+		return ChronoUnit.DAYS.between(fechaNacimiento,LocalDate.now());		
+	}
+	
 	public static void ejemplosFechas() {
 
 		Scanner sc = new Scanner(System.in);
@@ -64,10 +73,24 @@ public class ejemplos {
 		//String fechaCadena = sc.nextLine();
 		String fechaCadena = "09/11/2022";
 		//LocalDate fechaTransformada = LocalDate.parse(fechaCadena);  // formato estándar (aaaa-mm-dd)
-		LocalDate fechaTransformada = LocalDate.parse(fechaCadena,DateTimeFormatter.ofPattern("d/M/y"));  // Patrón más común en España
+		LocalDate fechaTransformada = LocalDate.parse(fechaCadena,DateTimeFormatter.ofPattern("dd/M/y"));  // Patrón más común en España
 		System.out.println(fechaTransformada);
 		//System.out.println("Hoy es: " + fechaTransformada.getDayOfMonth() + "/" + fechaTransformada.getMonthValue() + "/" + fechaTransformada.getYear());
 		System.out.println("Hoy es: " + devolverFechaEspanyol(fechaTransformada));
+		System.out.println("Hoy es el día del año: " + fecha.getDayOfYear());
+		String[] dias = {"Domingo", "Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado"};
+		System.out.println("Hoy es el día del semana: " + fecha.getDayOfWeek());  // En inglés por defecto
+		System.out.println("Hoy es el día del semana: " +  dias[fecha.getDayOfWeek().getValue()]);  // en castellano
+		
+		// Operaciones de fechas
+		System.out.println("Dentro de 10 días será: " + devolverFechaEspanyol(LocalDate.now().plusDays(55)));
+		System.out.println("Hace dos semanas era: " + devolverFechaEspanyol(LocalDate.now().minusWeeks(2)));
+		System.out.println("Llevo vividos " + diasVividos(LocalDate.of(1976, 7, 27)) + " días");
+		System.out.println("Llevo vividos " + Duration.between(LocalDateTime.of(1976, 7, 27,0,0,0),LocalDateTime.now()).toDays() + " días");   // más apropiada si hay horas
+		// Period devuelve el número de años, meses y días entre dos fechas
+		System.out.println("Tengo : " + Period.between(LocalDate.of(1976, 7, 27),LocalDate.now()).getYears() + " años");   // más apropiada si no hay horas
+		System.out.println("Tengo : " + Period.between(LocalDate.of(1976, 7, 27),LocalDate.now()).getMonths() + " meses");   // más apropiada si no hay horas
+		System.out.println("Tengo : " + Period.between(LocalDate.of(1976, 7, 27),LocalDate.now()).getDays() + " días");   // más apropiada si no hay horas
 		sc.close();
 	}
 	
