995fcae6d3387129e63aa668e142bd1442e83a46
fran <ctefran@hotmail.com>
2022-11-10 17:19:32 +0100

    Ficheros1

---
+++ ficheros/src/ficheros/Ejemplos.java
@@ -0,0 +1,48 @@
+package ficheros;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+
+public class Ejemplos {
+
+	public static void leerFicheroJava5() {
+		File archivo = null;
+		FileReader fr = null;
+		BufferedReader br = null;
+
+		try {
+			// Apertura del fichero y creacion de BufferedReader para poder
+			// hacer una lectura comoda (disponer del metodo readLine()).
+			//archivo = new File("C:\\Users\\Fran\\Documents\\EOI2022\\06_OctubreJavaIniciacionValenciaTarde\\EoiIniciacionOctubre2022\\ficheros\\funciones.txt");  // ruta absoluta
+			//archivo = new File("funciones.txt");  // ruta relativa
+			//archivo = new File("..\\padre.txt");  // ruta relativa
+			archivo = new File("files\\hijo.txt");  // ruta relativa
+			fr = new FileReader(archivo);
+			br = new BufferedReader(fr);
+
+			// Lectura del fichero
+			String linea;
+			while ((linea = br.readLine()) != null)
+				System.out.println(linea);
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			// En el finally cerramos el fichero, para asegurarnos
+			// que se cierra tanto si todo va bien como si salta
+			// una excepcion.
+			try {
+				if (null != fr) {
+					fr.close();
+				}
+			} catch (Exception e2) {
+				e2.printStackTrace();
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		leerFicheroJava5();
+	}
+
+}
---
+++ ficheros/src/ficheros/Ejercicios.java
@@ -0,0 +1,10 @@
+package ficheros;
+
+public class Ejercicios {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
