a55680e3d650c892ff1f472e7423ba36ae98f1e0
fran <ctefran@hotmail.com>
2022-11-10 19:02:05 +0100

    ejercicio1

--- ficheros/src/ficheros/Ejemplos.java
+++ ficheros/src/ficheros/Ejemplos.java
@@ -3,11 +3,14 @@ package ficheros;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.Arrays;
 
 public class Ejemplos {
 
@@ -50,15 +53,55 @@ public class Ejemplos {
 		}
 	}
 
+	
+	public static void escribirFichero5() {
+		FileWriter fichero = null;
+        PrintWriter pw = null;
+        try
+        {
+            fichero = new FileWriter("chuwiki.txt");
+            pw = new PrintWriter(fichero);
+
+            for (int i = 0; i < 10; i++)
+                pw.println("Linea " + i);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+           try {
+           // Nuevamente aprovechamos el finally para 
+           // asegurarnos que se cierra el fichero.
+           if (null != fichero)
+              fichero.close();
+           } catch (Exception e2) {
+              e2.printStackTrace();
+           }
+        }
+	}
+
+	
 	public static void leerFicheroJava8(String directorio, String nombreArchivo) throws IOException {
 		Files.readAllLines(Paths.get(directorio, nombreArchivo),Charset.defaultCharset())
 		.forEach(l->System.out.println(l));
 	}
 	
+	public static void escribirFicheroJava8(String directorio, String nombreArchivo,String[] lineas) throws IOException {
+		Files.write(Paths.get(directorio, nombreArchivo), Arrays.asList(lineas));
+	}
+	
+	public static void anyadirFicheroJava8(String directorio, String nombreArchivo,String[] lineas) throws IOException {
+		Files.write(Paths.get(directorio, nombreArchivo), Arrays.asList(lineas),StandardOpenOption.APPEND);
+	}
+	
+	
 	public static void main(String[] args) throws IOException {
 		//leerFicheroJava5();
 		//leerFicheroJava8("files","segundo.txt");
 		
+		
+		
+		// Lectura de ficheros
+		/*
 		leerFicheroJava8("","funciones.txt");  // para acceder a la carpeta del proyecto
 		leerFicheroJava8("files","hijo.txt");  // para acceder al hijo con ruta relativa
 		leerFicheroJava8("..","padre.txt");  // para acceder al padre con ruta relativa
@@ -66,6 +109,14 @@ public class Ejemplos {
 		String directorio = fichero.getParent();
 		String nombreArchivo = fichero.getName();
 		leerFicheroJava8(directorio,nombreArchivo);  // para acceder con ruta absoluta
+		 */
+		
+		// Escritura de ficheros
+		// escribirFichero5();
+		
+		String[] lineas = {"primera línea","segunda línea","tercera línea"};
+		//escribirFicheroJava8("files","escribir1.txt",lineas);
+		anyadirFicheroJava8("files","escribir1.txt",lineas);
 	}
 
 }
--- ficheros/src/ficheros/Ejercicios.java
+++ ficheros/src/ficheros/Ejercicios.java
@@ -1,9 +1,41 @@
 package ficheros;
 
-public class Ejercicios {
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.Arrays;
+import java.util.List;
 
-	public static void main(String[] args) {
-		
+public class Ejercicios {
+	
+	public static void leerFicheroJava8(String directorio, String nombreArchivo) throws IOException {
+		Files.readAllLines(Paths.get(directorio, nombreArchivo),Charset.defaultCharset())
+		.forEach(l->System.out.println(l));
+	}
+	
+	public static void almacenarLineasJava8(String directorio, String nombreArchivo) throws IOException {
+		List<String> lineasLista = Files.readAllLines(Paths.get(directorio, nombreArchivo),Charset.defaultCharset());
+		String[] lineas = lineasLista.stream().toArray(String[]::new);  // transforma la lista en un array de elementos
+		for(String linea : lineas)
+			System.out.println(linea);
+	}
+	
+	public static void escribirFicheroJava8(String directorio, String nombreArchivo,String[] lineas) throws IOException {
+		Files.write(Paths.get(directorio, nombreArchivo), Arrays.asList(lineas));
+	}
+	
+	public static void anyadirFicheroJava8(String directorio, String nombreArchivo,String[] lineas) throws IOException {
+		Files.write(Paths.get(directorio, nombreArchivo), Arrays.asList(lineas),StandardOpenOption.APPEND);
+	}
+	
+	public static void ejercicio1() throws IOException {
+		almacenarLineasJava8("","funciones.txt");
+	}
+
+	public static void main(String[] args) throws IOException {
+		ejercicio1();
 
 	}
 
